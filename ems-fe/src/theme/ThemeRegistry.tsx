15b030d10366239bb3ccd27da92c7c1bb2f1759b
Neplokhova <neplokhova.main@gmail.com>
2026-01-20 14:30:07 +0200

    fin

--- ems-fe/src/theme/ThemeRegistry.tsx
+++ ems-fe/src/theme/ThemeRegistry.tsx
@@ -1,17 +1,81 @@
 'use client';
 
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { theme } from './theme';
+import * as React from 'react';
+import { useServerInsertedHTML } from 'next/navigation';
+import { CacheProvider } from '@emotion/react';
+import createCache from '@emotion/cache';
+import type { EmotionCache } from '@emotion/cache';
+import type { SerializedStyles } from '@emotion/utils';
+import { ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
 
-export default function ThemeRegistry({
-  children,
-}: {
+import {theme} from './theme';
+
+function createEmotionCache(): EmotionCache {
+  // key має бути стабільний, prepend:true зазвичай найкраще для MUI
+  return createCache({ key: 'mui', prepend: true });
+}
+
+type ThemeRegistryProps = {
   children: React.ReactNode;
-}) {
+};
+
+export default function ThemeRegistry({ children }: ThemeRegistryProps) {
+  const [{ cache, flush }] = React.useState(() => {
+    const cache = createEmotionCache();
+
+    // Для сумісності з SSR інколи корисно
+    cache.compat = true;
+
+    let inserted: string[] = [];
+
+    const prevInsert = cache.insert.bind(cache);
+
+    // ВАЖЛИВО: типізуємо сигнатуру без any і без ...args
+    cache.insert = (
+      selector: string,
+      serialized: SerializedStyles,
+      sheet: EmotionCache['sheet'],
+      shouldCache: boolean
+    ) => {
+      if (cache.inserted[serialized.name] === undefined) {
+        inserted.push(serialized.name);
+      }
+      return prevInsert(selector, serialized, sheet, shouldCache);
+    };
+
+    const flush = () => {
+      const prev = inserted;
+      inserted = [];
+      return prev;
+    };
+
+    return { cache, flush };
+  });
+
+  useServerInsertedHTML(() => {
+    const names = flush();
+    if (names.length === 0) return null;
+
+    let styles = '';
+    for (const name of names) {
+      styles += cache.inserted[name];
+    }
+
+    return (
+      <style
+        data-emotion={`${cache.key} ${names.join(' ')}`}
+        dangerouslySetInnerHTML={{ __html: styles }}
+      />
+    );
+  });
+
   return (
-    <ThemeProvider theme={theme}>
-      <CssBaseline />
-      {children}
-    </ThemeProvider>
+    <CacheProvider value={cache}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </CacheProvider>
   );
 }
